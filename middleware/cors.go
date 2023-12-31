d7a8230e1ca01adc5a4f78f29fd1d92f5581f9d7
Asher <awaketai@gmail.com>
2023-12-31 10:48:27 +0800

    feat:cors middleware

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"errors"
+	"net/http"
 	"strings"
 	"time"
 
@@ -161,15 +162,132 @@ func DefaultConfig() Config {
 	}
 }
 
-// func Default() gin.HandlerFunc {
-// 	config := DefaultConfig()
-// 	config.AllowAllOrigins = true
-// 	return New(config)
-// }
+type cors struct {
+	allowAllOrigins          bool
+	allowCredentials         bool
+	allowOriginFunc          func(string) bool
+	allowOrigins             []string
+	normalHeaders            http.Header
+	preflightHeaders         http.Header
+	wildcardOrigins          [][]string
+	optionResponseStatusCode int
+}
 
-// func New(config Config) gin.HandlerFunc {
-// 	cors := newCors(config)
-// 	return func(c *gin.Context) {
-// 		cors.applyCors(c)
-// 	}
-// }
+func newCors(config Config) *cors {
+	if err := config.Validate(); err != nil {
+		panic(err.Error())
+	}
+	for _, origin := range config.AllowOrigins {
+		if origin == "*" {
+			config.AllowAllOrigins = true
+		}
+	}
+	if config.OptionResponseStatusCode == 0 {
+		config.OptionResponseStatusCode = http.StatusNoContent
+	}
+
+	return &cors{
+		allowOriginFunc:          config.AllowOriginFunc,
+		allowAllOrigins:          config.AllowAllOrigins,
+		allowCredentials:         config.AllowCredentials,
+		allowOrigins:             normalize(config.AllowOrigins),
+		normalHeaders:            generateNormalHeaders(config),
+		preflightHeaders:         generatePreflightHeaders(config),
+		wildcardOrigins:          config.parseWildcardRules(),
+		optionResponseStatusCode: config.OptionResponseStatusCode,
+	}
+}
+
+func (cors *cors) applyCors(c *gin.Context) {
+	origin := c.Request.Header.Get("Origin")
+	if len(origin) == 0 {
+		// request is not a CORS request
+		return
+	}
+	host := c.Request.Host
+
+	if origin == "http://"+host || origin == "https://"+host {
+		// request is not a CORS request but have origin header.
+		// for example, use fetch api
+		return
+	}
+
+	if !cors.validateOrigin(origin) {
+		c.AbortWithStatus(http.StatusForbidden)
+		return
+	}
+
+	if c.Request.Method == "OPTIONS" {
+		cors.handlePreflight(c)
+		defer c.AbortWithStatus(cors.optionResponseStatusCode)
+	} else {
+		cors.handleNormal(c)
+	}
+
+	if !cors.allowAllOrigins {
+		c.Header("Access-Control-Allow-Origin", origin)
+	}
+}
+
+func (cors *cors) validateWildcardOrigin(origin string) bool {
+	for _, w := range cors.wildcardOrigins {
+		if w[0] == "*" && strings.HasSuffix(origin, w[1]) {
+			return true
+		}
+		if w[1] == "*" && strings.HasPrefix(origin, w[0]) {
+			return true
+		}
+		if strings.HasPrefix(origin, w[0]) && strings.HasSuffix(origin, w[1]) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (cors *cors) validateOrigin(origin string) bool {
+	if cors.allowAllOrigins {
+		return true
+	}
+	for _, value := range cors.allowOrigins {
+		if value == origin {
+			return true
+		}
+	}
+	if len(cors.wildcardOrigins) > 0 && cors.validateWildcardOrigin(origin) {
+		return true
+	}
+	if cors.allowOriginFunc != nil {
+		return cors.allowOriginFunc(origin)
+	}
+	return false
+}
+
+func (cors *cors) handlePreflight(c *gin.Context) {
+	header := c.Writer.Header()
+	for key, value := range cors.preflightHeaders {
+		header[key] = value
+	}
+}
+
+func (cors *cors) handleNormal(c *gin.Context) {
+	header := c.Writer.Header()
+	for key, value := range cors.normalHeaders {
+		header[key] = value
+	}
+}
+
+// Default returns the location middleware with default configuration.
+func Default() gin.HandlerFunc {
+	config := DefaultConfig()
+	config.AllowAllOrigins = true
+	return New(config)
+}
+
+// New returns the location middleware with user-defined custom configuration.
+func New(config Config) gin.HandlerFunc {
+	cors := newCors(config)
+	return func(c *gin.Context) {
+		cors.applyCors(c)
+	}
+}
